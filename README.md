ba3df2a85ee5c20b71912a9f6d699a80b02a6c91
Willard <sven0215@hotmail.com>
2019-05-06 18:44:14 +0200

    real initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Gruppe Adler MTG
+
+Gruppe Adler Map Tile Generator (grad_mtg) is a intercept client which generates map tiles from A3 Maps.
---
+++ addons/mtg/config.cpp
@@ -0,0 +1,22 @@
+class CfgPatches {
+	class grad_mtg {
+		name = "Gruppe Adler Map Tiles Generator";
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 1.82;
+		requiredAddons[] = {"intercept_core"};
+		author = "Willard";
+		authors[] = {"Willard"};
+		url = "https://github.com/gruppe-adler";
+		version = "1.0";
+		versionStr = "1.0";
+		versionAr[] = {1,0};
+	};
+};
+class Intercept {
+    class Willard {
+        class grad_mtg {
+            pluginName = "grad_mtg";
+        };
+    };
+};
